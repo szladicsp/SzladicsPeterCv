e1e942e1497152053682d5559f4dea8c5e656b1c
Szladics Peter <szladicsp@gmail.com>
2018-11-30 20:26:27 +0100

    Add all

--- SzladicsPeterCv/public_html/js/index.js
+++ SzladicsPeterCv/public_html/js/index.js
@@ -9,4 +9,36 @@ window.onscroll = function() {
   prevScrollpos = currentScrollPos;
 }
 
- $('body').scrollspy({ target: '#navbarscroll' })
+ $('body').scrollspy({ target: '#navbarscroll' });
+ 
+ 
+ 
+ $(document).ready(function () {
+2
+  $('.animated').each(function (index) {
+3
+    $(this).delay($(this).data('delay')).queue(function () {
+4
+      $(this).addClass('animate-in');
+5
+    });
+6
+  });
+7
+});
+
+//Todo animate. css lásd animate uses
+
+$(document).ready(function animationHover(element, animation){
+    element = $(element);
+    element.hover(
+        function() {
+            element.addClass('animated ' + animation);        
+        },
+        function(){
+            //wait for animation to finish before removing classes
+            window.setTimeout( function(){
+                element.removeClass('animated ' + animation);
+            }, 2000);         
+        });
+});
